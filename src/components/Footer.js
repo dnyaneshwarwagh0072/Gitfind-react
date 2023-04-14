19ad2ec7fe0c2846f1e894bf9fe1a2621cb6bd72
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2023-04-14 19:43:25 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <div className="card footer">
         <h2>GitFind | Developed by DnyaneshwarWagh &amp; | &copy; 2023</h2>
-        <a href="https://github.com/dnyaneshwarwagh0072/gitfind" target="_blank" rel="noreferrer">Github Source Code</a>
+        <a href="https://github.com/dnyaneshwarwagh0072/Gitfind-react.git" target="_blank" rel="noreferrer">Github Source Code</a>
       </div>
     )
   }
