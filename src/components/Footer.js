c48e6a73afac1f0b1ff45ea160f2f17c8ddd3fce
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2023-04-14 19:40:11 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <div className="card footer">
+        <h2>GitFind | Developed by DnyaneshwarWagh &amp; | &copy; 2023</h2>
+        <a href="https://github.com/dnyaneshwarwagh0072/gitfind" target="_blank" rel="noreferrer">Github Source Code</a>
+      </div>
+    )
+  }
+  
+  export default Footer;
+  
